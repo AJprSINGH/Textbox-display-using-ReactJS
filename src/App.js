d171b3e9b74a4b07f10a82d0f327c550110842fe
AJprSINGH <ajit.12008255@lpu.in>
2023-01-19 20:46:48 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {Header} from './mycomponents/Header';
+import {AddTitle} from './mycomponents/AddTitle';
+import {Footer} from './mycomponents/Footer';
+import { useState,useEffect } from 'react';
+
 
 function App() {
+  const header = (title,regno)=>{
+    console.log("I am adding this title and regno", title,regno);
+    
+    const mytitle = {
+      title: title,
+      welcome: "Welcome to my college",
+      hey: "Hey!",
+      no: "Your regno. is",
+      regno: regno,
+    }
+    setFooter([...footer, mytitle]);
+    console.log(mytitle);
+  }
+  const [footer,setFooter] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header header={header}/>
+      <AddTitle footer={footer}/>
+      <Footer footer={footer}/>
+       <p>assignment done by suraj 12012825</p>
     </div>
   );
 }
